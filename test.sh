4fb8d0757c230b7e34dead8d47d2aea600bc3bdc
toof-jp <toof.jp.mail@gmail.com>
2020-10-26 22:22:46 +0900

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+
+  # gcc main.cpp
+  # ./9cc "$input" > tmp.s
+  # cc -o tmp tmp.s
+  # ./tmp
+  actual="$(echo "$input" | ./a.out)"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 3 3
+
+echo OK
